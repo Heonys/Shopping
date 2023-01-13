914759e1b356b80eeb2ed7fb0d026ccac12ec173
heon <siwmua0@naver.com>
2023-01-14 03:41:13 +0900

    [Shopping-APP] Feat: initalization

--- src/App.js
+++ src/App.js
@@ -1,21 +1,8 @@
 import React from "react";
 import "./index.css";
-import { Outlet } from "react-router-dom";
-import Header from "components/Header";
-import { AuthContextProvider } from "context/AuthContext";
-import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
 const App = () => {
-  const client = new QueryClient();
-
-  return (
-    <AuthContextProvider>
-      <QueryClientProvider client={client}>
-        <Header />
-        <Outlet />
-      </QueryClientProvider>
-    </AuthContextProvider>
-  );
+  return <div>app</div>;
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { RouterProvider } from "react-router-dom";
-import { router } from "./routes";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <RouterProvider router={router}>
-    <App />
-  </RouterProvider>
-);
+root.render(<App />);
